35bc0216022045d6ae535bf2624c65da7a3b0b26
Fauzan773 <80533403+Fauzan773@users.noreply.github.com>
2021-04-13 05:53:08 +0700

    Add files via upload

---
+++ Pertemuan 6/Teori/Mobil.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Transportasi;
+
+/**
+ *
+ * @author FAUZAN MAULANA ALFANY - A11.2019.12035
+ */
+public class Mobil {
+
+    public void Mobil(){
+        System.out.println("Ini adalah class Mobil...");
+    }
+    public void goStraight(){
+        System.out.println("Maju..");
+    }
+    public void turnLeft(){
+        System.out.println("Belok Kiri..");
+    }
+    public void turnRight(){
+        System.out.println("Belok Kanan..");
+    }
+}
