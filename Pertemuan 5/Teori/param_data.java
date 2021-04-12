f788045ba5adfca1ee7715b556f9dcfe3080e719
Fauzan773 <80533403+Fauzan773@users.noreply.github.com>
2021-04-13 05:35:38 +0700

    Add files via upload

---
+++ Pertemuan 5/Teori/param_data.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array_stack_queue;
+
+/**
+ *
+ * @author Fauzan Maulana Alfany
+ * A11.4418
+ * 
+ */
+public class param_data {
+    int[] param_data = new int[5];
+
+}
